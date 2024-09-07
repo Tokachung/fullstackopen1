aa0e1472b8eec16cd878bfeafc259877932e6210
tokachung <st1403@live.com>
2024-09-07 19:35:18 +1200

    cleaned up repository structure

---
+++ part1/src/main.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+// const refresh = () => {
+//     ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+// }
+
+// setInterval(() => {
+//     refresh()
+//     counter += 1
+// }, 1000)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
