b9162c9f0173c515d8f1d8714b8c9e0de9d21afb
tokachung <st1403@live.com>
2025-03-31 22:36:43 +1100

    testing mock functions

---
+++ part2/src/components/Note.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Note from './Note'
+
+test('renders content', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = vi.fn() // Create a mock function to simulate the toggleImportance function
+
+  render(<Note note={note} toggleImportance={mockHandler}/>)
+
+  const user = userEvent.setup() // Set up a session to interact with the rendered component
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1) // Check that the mock function was called once
+
+  // ...
+
+})
