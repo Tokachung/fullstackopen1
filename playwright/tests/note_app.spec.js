bdaece6bb760e6128aa353a599aefb65f15c5816
tokachung <st1403@live.com>
2025-04-03 23:18:07 +1100

    refactor test

--- playwright/tests/note_app.spec.js
+++ playwright/tests/note_app.spec.js
@@ -1,24 +1,23 @@
-const { test, expect, describe } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 
 describe('Note app', () => {
+    beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173')
+    })
 
     test('front page can be opened', async ({ page }) => {
-        await page.goto('http://localhost:5173')
     
         const locator = await page.getByText('Notes')
         await expect(locator).toBeVisible()
         await expect(page.getByText('Note app, made by Shawn & Department of Computer Science, University of Helsinki 2025')).toBeVisible()
     })
 
-    test('login form can be opened', async ({ page}) => {
-        await page.goto('http://localhost:5173')
-
+    test('login form can be opened', async ({ page }) => {
         await page.getByRole('button', { name: 'login' }).click()
-        await page.getByRole('textbox').first().fill('root')
-        await page.getByRole('textbox').last().fill('password')
+        await page.getByTestId('username').fill('root')
+        await page.getByTestId('password').fill('password')
         await page.getByRole('button', { name: 'login' }).click()
-
         await expect(page.getByText('root user logged-in')).toBeVisible()
     })
 
