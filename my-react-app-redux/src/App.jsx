c2d0faee207ba31e97b26db2fcb88a844eae7f5f
Shawn Tang <st1403@live.com>
2025-06-20 15:43:00 +1000

    6b start, moved functionality of NewNote to its own jsx component

--- my-react-app-redux/src/App.jsx
+++ my-react-app-redux/src/App.jsx
@@ -1,16 +1,16 @@
 import { createNote, toggleImportanceOf } from './reducers/noteReducer'
 import { useSelector, useDispatch } from 'react-redux'
+import NewNote from './components/NewNote'
 
 const App = () => {
+  const filteredSelected = (value) => {
+    console.log(value)
+  }
+
+
+
   const dispatch = useDispatch()
   const notes = useSelector(state => state)
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    dispatch(createNote(content))
-  }
 
   const toggleImportance = (id) => {
     dispatch(toggleImportanceOf(id))
@@ -18,17 +18,14 @@ const App = () => {
 
   return(
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" /> 
-        <button type="submit">add</button>
-      </form>
+      <NewNote />
       <ul>
         {notes.map(note=>
           <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
-        </ul>
+      </ul>
     </div>
   )
 }
