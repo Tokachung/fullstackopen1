5faa059ee113d2bde738a25dbe07da2e5a34c71e
Shawn Tang <st1403@live.com>
2025-06-20 17:00:45 +1000

    6b Clicking the different radio buttons changes the state of the store's filter property

--- my-react-app-redux/src/App.jsx
+++ my-react-app-redux/src/App.jsx
@@ -1,46 +1,16 @@
-import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
-import { useSelector, useDispatch } from "react-redux";
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
+import VisibilityFilter from "./components/VisibilityFilter";
 
 const App = () => {
-	const filteredSelected = (value) => {
-		console.log(value);
-	};
 
 	return (
 		<div>
 			<NewNote />
-
-			<div>
-				all
-				<input
-					type="radio"
-					name="filter"
-					onChange={() => {
-						filteredSelected("ALL");
-					}}
-				/>
-				important
-				<input
-					type="radio"
-					name="filter"
-					onChange={() => {
-						filteredSelected("IMPORTANT");
-					}}
-				/>
-				nonimportant
-				<input
-					type="radio"
-					name="filter"
-					onChange={() => {
-						filteredSelected("NONIMPORTANT");
-					}}
-				/>
-			</div>
+      <VisibilityFilter />
 			<Notes />
 		</div>
 	);
 };
 
-export default App;
+export default App
