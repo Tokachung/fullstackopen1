2cd2d2f958bae04ecffa9c8a579a5631d530f10b
tokachung <st1403@live.com>
2025-07-01 18:52:41 +1000

    Sending data to the backend

--- my-react-app-redux/src/App.jsx
+++ my-react-app-redux/src/App.jsx
@@ -1,8 +1,19 @@
 import NewNote from "./components/NewNote";
 import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
+import { useEffect } from "react"; // Import useEffect to handle side effects
+import { useDispatch } from "react-redux";
+import noteService from "./services/notes";
+import { setNotes } from "./reducers/noteReducer"; // Import the action to set notes
 
 const App = () => {
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		noteService.getAll().then(notes => dispatch(setNotes(notes)))
+	}, [])
+
 	return (
 		<div>
 			<NewNote />
--- my-react-app-redux/src/main.jsx
+++ my-react-app-redux/src/main.jsx
@@ -2,11 +2,9 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
-
-import noteReducer from "./reducers/noteReducer";
+import noteReducer, { setNotes } from './reducers/noteReducer';
 import filterReducer, { filterChange } from "./reducers/filterReducer";
-
-
+import noteService from "./services/notes";
 
 const store = configureStore({
 	reducer: {
@@ -14,6 +12,10 @@ const store = configureStore({
 		filter: filterReducer,
 	}
 })
+
+noteService.getAll().then(notes => 
+	store.dispatch(setNotes(notes))
+)
 
 console.log(store.getState())
 
