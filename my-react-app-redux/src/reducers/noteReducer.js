4f514eaa725b126597549d8daf7cdd00978b8288
tokachung <st1403@live.com>
2025-07-04 17:09:09 +1000

    tidyup and add thunk

--- my-react-app-redux/src/reducers/noteReducer.js
+++ my-react-app-redux/src/reducers/noteReducer.js
@@ -1,16 +1,5 @@
 import { createSlice, current } from "@reduxjs/toolkit";
-
-// const initialState = [
-// 	{
-// 		content: "reducer defines how redux store works",
-// 		important: "true",
-// 		id: 1,
-// 	},
-// 	{ 
-// 		content: "state of store can contain any data", 
-// 		important: false, 
-// 		id: 2 },
-// ];
+import noteService from '../services/notes';
 
 const generateId = () => 
 	Number((Math.random() * 1000000).toFixed(0));
@@ -45,4 +34,11 @@ const noteSlice = createSlice({
 })
 
 export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+	return async dispatch => {
+		const notes = await noteService.getAll()
+		dispatch(setNotes(notes))
+	}
+}
 export default noteSlice.reducer
