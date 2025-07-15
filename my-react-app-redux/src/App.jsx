8c3e410b0a4327d0e6f2e6b22f6d5055e5046f18
Shawn Tang <st1403@live.com>
2025-07-16 08:20:40 +1000

    complete thunk

--- my-react-app-redux/src/App.jsx
+++ my-react-app-redux/src/App.jsx
@@ -3,16 +3,13 @@ import Notes from "./components/Notes";
 import VisibilityFilter from "./components/VisibilityFilter";
 import { useEffect } from "react"; // Import useEffect to handle side effects
 import { useDispatch } from "react-redux";
-import noteService from "./services/notes";
-import { setNotes } from "./reducers/noteReducer"; // Import the action to set notes
 
 const App = () => {
-
 	const dispatch = useDispatch()
 
 	useEffect(() => {
-		noteService.getAll().then(notes => dispatch(setNotes(notes)))
-	}, [])
+		dispatch(initializeNotes())
+	}, [dispatch])
 
 	return (
 		<div>
